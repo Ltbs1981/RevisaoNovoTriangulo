fafeb2ad91ffa3ed5b69aecac41a4cd0bf5c9b5a
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-22 11:44:26 -0300

    Programa final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RevisaoNovoTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo primeiroTriangulo = new Triangulo();
+            primeiroTriangulo.lado1 = 5.55;
+            primeiroTriangulo.lado2 = 7.77;
+            primeiroTriangulo.lado3 = 3.33;
+
+            double areaPrimeiroTriangulo = primeiroTriangulo.CalcularArea();
+
+            Triangulo segundoTriangulo = new Triangulo();
+            segundoTriangulo.lado1 = 4.44;
+            segundoTriangulo.lado2 = 6.66;
+            segundoTriangulo.lado3 = 8.88;
+
+            double areaSegundoTriangulo = segundoTriangulo.CalcularArea();
+
+            Console.WriteLine($"O primeiro Triângulo tem a área de: {areaPrimeiroTriangulo}");
+            Console.WriteLine($"O segundo Triângulo tem a área de: {areaSegundoTriangulo}");
+
+            if (areaPrimeiroTriangulo > areaSegundoTriangulo)
+            {
+                Console.WriteLine("O primeiro triângulo é maior que o segundo.");
+            }
+            else if (areaPrimeiroTriangulo < areaSegundoTriangulo)
+            {
+                Console.WriteLine("O segundo triângulo é maior que o primeiro.");
+            }
+            else
+            {
+                Console.WriteLine("Os dois triângulos têm a mesma área.");
+            }
+
+        }
+    }
+}
--- Triangulo.cs
+++ Triangulo.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RevisaoNovoTriangulo
+﻿namespace RevisaoNovoTriangulo
 {
-    internal class Triangulo
+    public class Triangulo
     {
+        // Definindo os membros da classe
+        public double lado1;
+        public double lado2;
+        public double lado3;
+
+        // Método para calcular a área do triângulo
+        public double CalcularArea()
+        {
+            double perimetro = (lado1 + lado2 + lado3) / 2;
+            double area = Math.Sqrt(perimetro * (perimetro - lado1) * (perimetro - lado2) * (perimetro - lado3));
+
+            return area;
+        }
     }
 }
